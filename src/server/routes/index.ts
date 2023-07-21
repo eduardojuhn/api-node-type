aec91b8d42897e4e9d720c0dbd0f698309832682
Eduardo <eduardo.shimizu@gicbrasil.com>
2023-07-21 15:57:51 -0300

    folder structure

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -1,15 +1,17 @@
-import { Router } from 'express';
+import {Router} from 'express';
+import {StatusCodes} from 'http-status-codes';
 
 const router = Router();
 
 
-router.get('/teste', (req, res) => {
+router.get('/', (_, res) => {
     return res.send('Oie');
 });
 
-
-
-
+router.post('/teste/:id', (req, res) => {
+    console.log(req.query.teste);
+    return res.status(StatusCodes.UNAUTHORIZED).json(req.body);
+});
 
 
 
