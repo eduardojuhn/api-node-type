4c7410a322c58938330db61b3312f2bcb27e1bb2
Eduardo <eduardo.shimizu@gicbrasil.com>
2023-07-27 16:24:05 -0300

    Update by id

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -13,6 +13,7 @@ router.get('/', (_, res) => {
 
 router.get('/cidades', CidadesController.getAllValidation, CidadesController.getAll);
 router.get('/cidades/:id', CidadesController.getByIdValidation, CidadesController.getById);
+router.put('/cidades/:id', CidadesController.updateByIdValidation, CidadesController.updateById);
 router.post('/cidades', CidadesController.createValidation, CidadesController.create);
 
 
